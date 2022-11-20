4ad02c4ef93d7d6fb995144e1f2e694c344d6299
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-11-20 13:02:22 +0530

    Update write_new_unp.ino

--- RFID Transaction/write_new_unp.ino
+++ RFID Transaction/write_new_unp.ino
@@ -12,16 +12,18 @@ void read_write_new_unp()
   MFRC522::StatusCode status;
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
     return;
   }
 
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
     return;
   }
 
-  Serial.println(F("**Card Detected:**"));
+  //Serial.println(F("**Card Detected:**"));
 
   byte buffer1[18];
 
@@ -30,14 +32,16 @@ void read_write_new_unp()
 
   //------------------------------------------- GET POINTS READING
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("Authentication failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
   }
 
   status = mfrc522.MIFARE_Read(block, buffer1, &len);
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("Reading failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
@@ -48,14 +52,14 @@ void read_write_new_unp()
   {
     if (buffer1[i] != 32)
     {
-      Serial.write(buffer1[i]);
+      //Serial.write(buffer1[i]);
       pts = pts + char((buffer1[i]));
     }
   }
-  Serial.print(" ");
+  //Serial.print(" ");
   balance_pts = pts.toInt();
 
-  Serial.println(F("\n**End Reading**\n"));
+  //Serial.println(F("\n**End Reading**\n"));
 
   delay(1000); //change value if you want to read cards faster
 
@@ -65,12 +69,7 @@ void read_write_new_unp()
   //HERE WE WRITE AFTER READING
   
   byte buffer[34];
-  //byte block;
-  //MFRC522::StatusCode status;
-  //byte len;
-
-  //Serial.setTimeout(20000L) ;     // wait until 20 seconds for input from serial
-  // Enter new unp automatically
+  
   new_balance_ptss = String(new_balance_pts);
   len = new_balance_ptss.length();
   len = len+1;
@@ -82,7 +81,8 @@ void read_write_new_unp()
   block = 4;
   //Serial.println(F("Authenticating using key A..."));
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("PCD_Authenticate() failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
@@ -90,7 +90,8 @@ void read_write_new_unp()
 
   // Write block
   status = mfrc522.MIFARE_Write(block, buffer, 16);
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("MIFARE_Write() failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
@@ -100,7 +101,8 @@ void read_write_new_unp()
   block = 5;
   //Serial.println(F("Authenticating using key A..."));
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("PCD_Authenticate() failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
@@ -108,7 +110,8 @@ void read_write_new_unp()
 
   // Write block
   status = mfrc522.MIFARE_Write(block, &buffer[16], 16);
-  if (status != MFRC522::STATUS_OK) {
+  if (status != MFRC522::STATUS_OK) 
+  {
     Serial.print(F("MIFARE_Write() failed: "));
     Serial.println(mfrc522.GetStatusCodeName(status));
     return;
@@ -116,7 +119,7 @@ void read_write_new_unp()
   else Serial.println(F("MIFARE_Write() success: "));
 
 
-  Serial.println(" ");
+  //Serial.println(" ");
   mfrc522.PICC_HaltA(); // Halt PICC
   mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
 }
