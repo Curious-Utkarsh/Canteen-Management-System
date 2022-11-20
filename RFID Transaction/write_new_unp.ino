87f44a669eb89cb5892fe2d7b644c4a35dda7484
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-11-20 11:25:49 +0530

    Update write_new_unp.ino

--- RFID Transaction/write_new_unp.ino
+++ RFID Transaction/write_new_unp.ino
@@ -1,8 +1,15 @@
-void write_new_unp()
+void read_write_new_unp()
 {
+  //FIRST WE READ HERE
+  
   // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
   MFRC522::MIFARE_Key key;
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+
+  //some variables we need
+  byte block;
+  byte len;
+  MFRC522::StatusCode status;
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
@@ -14,20 +21,53 @@ void write_new_unp()
     return;
   }
 
-//  Serial.print(F("Card UID:"));    //Dump UID
-//  for (byte i = 0; i < mfrc522.uid.size; i++) 
-//{
-//    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-//    Serial.print(mfrc522.uid.uidByte[i], HEX);
-  //}
-//  Serial.print(F(" PICC type: "));   // Dump PICC type
-//  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-//  Serial.println(mfrc522.PICC_GetTypeName(piccType));
+  Serial.println(F("**Card Detected:**"));
 
+  byte buffer1[18];
+
+  block = 4;
+  len = 18;
+
+  //------------------------------------------- GET POINTS READING
+  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Authentication failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  status = mfrc522.MIFARE_Read(block, buffer1, &len);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("Reading failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return;
+  }
+
+  //PRINT BALANCE POINTS
+  for (uint8_t i = 0; i < 16; i++)
+  {
+    if (buffer1[i] != 32)
+    {
+      Serial.write(buffer1[i]);
+      pts = pts + char((buffer1[i]));
+    }
+  }
+  Serial.print(" ");
+  balance_pts = pts.toInt();
+
+  Serial.println(F("\n**End Reading**\n"));
+
+  delay(1000); //change value if you want to read cards faster
+
+  new_balance_pts = int(balance_pts - deduct_pts);
+
+  
+  //HERE WE WRITE AFTER READING
+  
   byte buffer[34];
-  byte block;
-  MFRC522::StatusCode status;
-  byte len;
+  //byte block;
+  //MFRC522::StatusCode status;
+  //byte len;
 
   //Serial.setTimeout(20000L) ;     // wait until 20 seconds for input from serial
   // Enter new unp automatically
