92ff45aeae2ad788bac8c28332a8bdaae23fcc9e
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-11-20 13:01:12 +0530

    Update RFID_MONEY_TRANSACTION.ino

--- RFID Transaction/RFID_MONEY_TRANSACTION.ino
+++ RFID Transaction/RFID_MONEY_TRANSACTION.ino
@@ -12,58 +12,79 @@ int deduct_pts = 0;
 String deduct_ptss = "";
 int new_balance_pts = 0;
 String new_balance_ptss = "";
+String ch = "";
+int choice = 0;
 
 void setup() 
 {
   Serial.begin(9600);        // Initialize serial communications with the PC
-  //while (!Serial); 
   SPI.begin();               // Init SPI bus
   mfrc522.PCD_Init();        // Init MFRC522 card
 }
 
 void loop()
 {
-  Serial.println("PUT CARD to Write Points");
-  delay(3000);
-  write_unp();
-  Serial.println("REMOVE CARD");
-  delay(4000);
+  Serial.println(".....PRESS '1' TO ADD UNP TO CARD.....");
+  Serial.println(".....PRESS '2' TO CHECK BALANCE UNP IN CARD.....");
+  Serial.println(".....PRESS '3' TO MAKE TRANSACTION.....");
+  Serial.println();
 
-  Serial.println("Enter Points to Deduct");
-  while(Serial.available()== 0)
+  while(Serial.available() == 0)
   {}
-  deduct_ptss = Serial.readStringUntil('#');
-  deduct_pts = deduct_ptss.toInt();
-  Serial.print("POINTS TO BE DEDUCTED : ");
-  Serial.println(deduct_pts);
-  delay(1000);
-   
-  Serial.println("PUT CARD to Transacte");
-  delay(3000);
-  read_write_new_unp();
-  //Serial.print("BALACE POINTS IN CARD : ");
-  //Serial.println(balance_pts);
-  //delay(2000);
-  //Serial.println("REMOVE CARD");
 
-//  new_balance_pts = int(balance_pts - deduct_pts);
-  //Serial.print("NEW BALANCE POINTS AFTER DEDN : ");
-  //Serial.println(new_balance_pts);
-  //delay(2000);
-  //Serial.println("Writing New Points to Card");
-  //delay(3000);
-  //Serial.println("Put Card to write new balance points after deduction");
-  //delay(3000);
-  //write_new_unp();
-  Serial.println("REMOVE CARD");
-  delay(3000);
-  Serial.println("PUT CARD TO READ NEW UNP");
-  delay(3000);
+  ch = Serial.readStringUntil('#');
+  choice = ch.toInt();
+
+  switch(choice)
+  {
+    case 1:
+    Serial.println("PLACE CARD ON SCANNER TO ADD UNP  #");
+    delay(3000);
+    write_unp();
+    Serial.println("UNP ADDED");
+    delay(1000);
+    Serial.println("REMOVE CARD");
+    Serial.println();
+    delay(4000);
+    break;
+
+    case 2:
+    Serial.println("PLACE CARD ON SCANNER TO CHECK BALANCE UNP");
+    delay(3000);
+    read_unp();
+    Serial.print("BALANCE UNP IN CARD : ");
+    Serial.println(balance_pts);
+    delay(1000);
+    Serial.println("REMOVE CARD");
+    Serial.println();
+    delay(4000);
+    break;
+
+    case 3:
+    Serial.println("ENTER UNP TO DEDUCT  #");
+    while(Serial.available()== 0)
+    {}
+    deduct_ptss = Serial.readStringUntil('#');
+    deduct_pts = deduct_ptss.toInt();
+    Serial.print("POINTS TO BE DEDUCTED : ");
+    Serial.println(deduct_pts);
+    delay(1000);
+    
+    Serial.println("PLACE CARD ON SCANNER TO MAKE TRANSACTION");
+    delay(3000);
+    read_write_new_unp();
+    Serial.println("TRANSACTION MADE SUCCESSFULLY");
+    delay(1000);
+    Serial.println("REMOVE CARD");
+    Serial.println();
+    delay(3000);
+    break;
+
+    default :
+    Serial.println("INVALID CHOICE");
+    Serial.println();
+    break; 
+  }
   pts=""; // this line is very important
-  read_unp();
-  Serial.print("NEW BALANCE POINTS : ");
-  Serial.println(balance_pts);
-  Serial.println("REMOVE CARD FINALLY");
-  delay(5000);
-  exit(0);
+
 }
