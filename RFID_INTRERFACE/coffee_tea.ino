ed7dd6c2c63d8909fa54e23ad937bf9b5a9189ec
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-12-04 00:19:56 +0530

    Update coffee_tea.ino

--- RFID_INTRERFACE/coffee_tea.ino
+++ RFID_INTRERFACE/coffee_tea.ino
@@ -1,10 +1,18 @@
 void coffee_tea()
 {
-  u8g2.setFont(u8g2_font_6x10_tr);
   while(true)
   {
-  u8g2.clearBuffer();          // clear the internal memory
-  u8g2.drawStr(50,10,"Hello World!");  // write something to the internal memory
-  u8g2.sendBuffer();          // transfer internal memory to the display
+    u8g2.clearBuffer();         
+    u8g2.setFont(u8g2_font_6x10_tr); 
+    u8g2.drawStr(30,10,"SELECT ITEM");  
+    u8g2.drawLine(5, 12, 122, 12);
+    Cursor();
+    u8g2.drawStr(8, 25, "COFFEE         10/-\n");
+    u8g2.drawStr(8, 35, "TEA            10/-\n");
+    u8g2.drawStr(8, 45, "MILK           10/-");
+    u8g2.sendBuffer();   
   }
 }
+
+
+
