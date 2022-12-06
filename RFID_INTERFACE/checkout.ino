25942e7b74d711e7e94ddd8cc8a7722c007af9db
Utkarsh <105288469+Curious-Utkarsh@users.noreply.github.com>
2022-12-07 01:23:38 +0530

    Update checkout.ino

--- RFID_INTERFACE/checkout.ino
+++ RFID_INTERFACE/checkout.ino
@@ -1,31 +1,38 @@
 void checkout()
 {
-  char cnt_ch[10];    
-  char cnt_f[10];
-  itoa(cnt, cnt_ch, 10); 
-  itoa(flag, cnt_f, 10); 
+  char ch_q[10];    
+  char ch_f[10]; // If error in printing comes then increase buffer size...
+  char ch_p[10];
+  itoa(q[0], ch_q, 10);  
+  itoa(p[0], ch_p, 10);
+  f[0].toCharArray(ch_f, 10);
+  
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_6x10_tr); 
   u8g2.drawStr(35,10,"YOUR CART"); 
   u8g2.drawLine(5, 12, 122, 12);
   u8g2.setFont(u8g2_font_5x8_tr);
   
-  u8g2.drawStr(8, 25, "COFFEE         10/-\n");
+  u8g2.drawStr(8, 25, ch_f);
+  u8g2.drawStr(82, 25, ch_p);
+  u8g2.setCursor(93, 25);
+  u8g2.print("/-");
+  
   u8g2.drawStr(110, 25, "x\n");
   u8g2.setCursor(115, 25);
-  u8g2.print(cnt_ch);
+  u8g2.print(ch_q);
 
   joyStick();
 
-  if(button == "r")
+  if(button == 'r')
   {
     xCnt = 1;
-    button = "";
+    button = ' ';
   }
-  if(button == "l")
+  if(button == 'l')
   {
     xCnt = 0;
-    button = "";
+    button = ' ';
   }  
   u8g2.setFontMode(1);
   u8g2.setDrawColor(1);
